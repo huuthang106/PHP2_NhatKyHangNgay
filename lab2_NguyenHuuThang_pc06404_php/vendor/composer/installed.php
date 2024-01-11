e118e01f1f5ac6893d0e10d2285bb3c31d2b43e9
huuthang106 <thangcachep106@gmail.com>
2024-01-11 15:35:50 +0700

    [php] xong bai 3

---
+++ lab2_NguyenHuuThang_pc06404_php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c757cd88e9fcc9bd5f46d2e8726ee500bfeaa095',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c757cd88e9fcc9bd5f46d2e8726ee500bfeaa095',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
