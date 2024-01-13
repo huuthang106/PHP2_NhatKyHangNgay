dd67f8ec0ec2f0d938fd7c1bb5278002f9f209fc
huuthang106 <thangcachep106@gmail.com>
2024-01-13 21:11:55 +0700

    final

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'admin/nhatky',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a8ec3232e3122fb5cb05619275a3cf8038f6ee3d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/nhatky' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a8ec3232e3122fb5cb05619275a3cf8038f6ee3d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
